fbccf70b062aa9834f6900098250d946854d8148
Alex Cooper <alex@acooper.org>
2021-07-08 21:57:56 +1000

    Update class diagram

--- ploo/__init__.py
+++ ploo/__init__.py
@@ -8,33 +8,49 @@ Design information
 
 Class diagram::
 
-    ┌──────────┐     ┌──────────────────────┐     ┌───────────────────────┐
-    │ CVKernel │     │  CrossValidatedModel │     │ CrossValidationScheme │
-    │          │     │                      │     │                       │
-    │          │has-a│  * potential         │has-a│ * __iter__            │
-    │          ├─────►                      ├─────►                       │
-    │          │     │  * num_folds         │     │ * mask_for            │
-    └──────────┘     │                      │     │                       │
-                     │  * initial_param     │     │ * coordinates_for     │
-                     │                      │     │                       │
-                     └───────────────┬──────┘     └───────────────────────┘
-                                has-a│
-                     ┌───────────────▼──────┐
-                     │  Model               │
-                     │                      │
-                     │  * log_likelihood    │
-                     │                      │
-                     │  * log_prior         │
-                     │                      │
-                     │  * initial_param     │
-                     │                      │
-                     └──────────────────────┘
+┌─────────────────────────────┐           ┌─────────────────────┐
+│ Model                       │           │ arviz.InferenceData │
+├─────────────────────────────┤           └───────────▲─────────┘
+│                             │                       │inherits
+│ + potential()               │instantiate┌───────────┴─────────┐
+│ + inference()───────────────┼──────────►│ _Posterior          │
+│                             │           ├─────────────────────┤
+│ Abstract methods            │           │ + post_draws        │
+│                             │        ┌──┼─+ cross_validate()  │
+│ # log_likelihood()          │        │  │ + summarize()       │
+│ # log_prior()               │        │  │ ...Arviz methods... │
+│ # cond_log_pred()           │        │  └─────────────────────┘
+│ # initial_param()           │        │instantiate
+│ # to_unconstrained_params() │        │  ┌─────────────────────┐
+│ # to_constrained_params()   │        └─►│ CrossValidation     │
+│                             │           ├─────────────────────┤
+└─────────────────────────────┘           │ + scheme            │
+                                          │ + elpd              │
+                        ┌─────────────────┤ + elpd_se           │
+                        │ aggregate       │ + trace_plots()     │
+                        │                 │ + densities()       │
+                        │                 └─────────────────────┘
+    ┌───────────────────┴───┐
+    │ CrossValidationScheme │
+    ├───────────────────────┤
+    │ + mask_for()          │
+    │ + coordinates_for()   │
+    │ + pred_index_array()  │
+    │ + mask_array()        │
+    │ + summary_array()     │
+    │                       │
+    └▲──────▲────────▲─────▲┘
+     │      │        │     │
+     │      │        │     │
+┌────┴┐ ┌───┴─┐ ┌────┴──┐ ┌┴────┐
+│ LOO │ │ LFO │ │ KFold │ │ LGO │
+└─────┘ └─────┘ └───────┘ └─────┘
 
 """
 
 # flake8: noqa
 from .compare import compare
-from .cv import LFO, LOO, KFold
+from .cv import LFO, LGO, LOO, KFold
 from .hmc import WarmupResults, warmup
 from .model import Model
 from .transforms import IntervalTransform, LogTransform
