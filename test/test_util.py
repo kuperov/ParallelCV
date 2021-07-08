741fc3557fee03b2a8aaf9cb7f5f055cbf6f045f
Alex Cooper <alex@acooper.org>
2021-07-09 09:00:42 +1000

    Fix non-running test

--- test/test_util.py
+++ test/test_util.py
@@ -3,14 +3,20 @@
 Confidential code not for distribution.
 Alex Cooper <alex@acooper.org>
 """
-import unittest
+from test.util import TestCase
 
 from ploo.util import Timer
 
 
-class TestTimer(unittest.TestSuite):
+class TestTimer(TestCase):
+    """Check utility classes behave as expected"""
+
     def test_timer(self):
+        """We don't apply any delay, but python is slow enough for measureable
+        time to elapse between creating and evaluating the timer
+        """
         t = Timer()
-        elapsed = t.elapsed_sec()
-        self.assertTrue(elapsed > 0 and elapsed < 1e-4)
-        self.assertEqual(str(t), "0.0 sec")
+        elapsed = t.sec
+        self.assertGreater(elapsed, 0.0)
+        self.assertLess(elapsed, 1e-4)
+        self.assertEqual(str(t), "0.0 sec")  # by rounding
