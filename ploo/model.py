Off-by-one bug in radon model

ploo/model.py
@@ -13,6 +13,7 @@ import chex
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
+from jax import devices
 from jax import numpy as jnp
 from jax import random, vmap
 from scipy import stats as sst
@@ -39,6 +40,15 @@ CVFold = Union[int, Tuple[int, int]]
 _ARVIZ_PLOT = [name for name in dir(az) if name.startswith("plot_")]
 _ARVIZ_OTHER = ["summary", "ess", "loo"]
 _ARVIZ_METHODS = _ARVIZ_PLOT + _ARVIZ_OTHER
+
+
+def _print_devices():
+    """Print summary of available devices to console."""
+    device_list = [f"{d.device_kind} ({d.platform}{d.id})" for d in devices()]
+    if len(device_list) > 0:
+        print(f'Detected devices: {", ".join(device_list)}')
+    else:
+        print("Only CPU is available. Check cuda/cudnn library versions.")
 
 
 class _Posterior(az.InferenceData):
@@ -71,8 +81,7 @@ class _Posterior(az.InferenceData):
         self.draws = post_draws[first_param].shape[1]
         self.warmup_res = warmup_res
         self.rng_key = rng_key
-        self.chain_divergences = accumulator.divergence_count
-        self.total_divergences = jnp.sum(accumulator.divergence_count)
+        self.accumulator = accumulator
         # construct xarrays for ArviZ
         post_draw_map = {}
         coords = {  # gets updated for
@@ -92,6 +101,28 @@ class _Posterior(az.InferenceData):
         posterior = xr.Dataset(post_draw_map, coords=coords)
         super().__init__(posterior=posterior)
 
+    @property
+    def chain_divergences(self) -> int:
+        """Divergence count by chain"""
+        return self.accumulator.divergence_count
+
+    @property
+    def total_divergences(self) -> int:
+        """Total number of divergences, summed across chains"""
+        return jnp.sum(self.accumulator.divergence_count)
+
+    @property
+    def avg_acceptance_rate(self) -> float:
+        """Average acceptance rate across all chains, as proportion in [0,1]"""
+        return float(
+            jnp.sum(self.accumulator.accepted_count) / self.chains / self.draws
+        )
+
+    @property
+    def acceptance_rates(self) -> float:
+        """Acceptance rate by chains, as proportion in [0,1]"""
+        return self.accumulator.accepted_count / self.draws
+
     def __str__(self) -> str:
         title = f"{self.model.name} inference summary"
         arg0 = next(iter(self.post_draws))
@@ -100,7 +131,8 @@ class _Posterior(az.InferenceData):
             title,
             "=" * len(title),
             "",
-            f"{iters*chains:,} draws from {iters:,} iterations on {chains:,} chains" "",
+            f"{iters*chains:,} draws from {iters:,} iterations on {chains:,} chains",
+            f"{self.total_divergences} divergences, {self.avg_acceptance_rate*100}% acceptance rate",
         ] + [self._post_table()]
         return "\n".join(desc_rows)
 
@@ -546,7 +578,10 @@ class Model:
         """
         rng_key = random.PRNGKey(seed)
         warmup_key, inference_key, post_key = random.split(rng_key, 3)
-
+        draws, warmup_steps = int(draws), int(warmup_steps)
+        print("Full-data posterior inference")
+        print("=============================")
+        _print_devices()
         if warmup_results:
             print("Skipping warmup")
         else:
@@ -563,6 +598,10 @@ class Model:
                 f"      {warmup_steps} warmup draws took {timer}"
                 f" ({warmup_steps/timer.sec:.1f} iter/sec)."
             )
+            print(
+                f"      Step size {warmup_results.step_size:.4f}, "
+                f"integration length {warmup_results.int_steps} steps."
+            )
 
         print(
             f"HMC for {draws*chains:,} full-data draws "
@@ -575,6 +614,8 @@ class Model:
         divergent_chains = jnp.sum(accum.divergence_count > 0)
         if divergent_chains > 0:
             print(f"      WARNING: {divergent_chains} divergent chain(s).")
+        accept_rate_pc = float(100 * jnp.mean(accum.accepted_count) / draws)
+        print(f"      Average HMC acceptance rate {accept_rate_pc:.1f}%.")
         # map positions back to model coordinates
         position_model = vmap(self.to_model_params)(states.position)
         # want axes to be (chain, draws, ... <variable dims> ...)

ploo/models/radon.py
@@ -18,7 +18,9 @@ import requests
 from jax import numpy as jnp
 
 from ploo import Model
+from ploo.hmc import InfParams
 from ploo.model import ModelParams
+from ploo.transforms import LogTransform
 
 _DATA_DIR = os.path.join(os.path.dirname(__file__), "radon_data")
 _DATA_FILE = os.path.join(_DATA_DIR, "radon_all.json")
@@ -49,7 +51,7 @@ def _load_data() -> Dict[str, chex.ArrayDevice]:
         data = json.load(data_file)
     N, J = data["N"], data["J"]
     log_radon = jnp.array(data["log_radon"])
-    county_index = jnp.array(data["county_idx"])
+    county_index = jnp.array(data["county_idx"]) - 1  # zero-based
     floor_measure = jnp.array(data["floor_measure"])
     assert county_index.shape == (N,) and jnp.max(county_index) == J
     assert floor_measure.shape == (N,)
@@ -68,6 +70,8 @@ class RadonCountyIntercept(Model):
 
     .. _[1]: https://github.com/stan-dev/posteriordb/blob/master/posterior_database/models/stan/radon_county_intercept.stan
     """  # noqa: B950  # pylint: disable=line-too-long
+
+    sigma_tx = LogTransform()
 
     def __init__(self) -> None:
         """Creates radon county intercept model
@@ -110,4 +114,18 @@ class RadonCountyIntercept(Model):
             "sigma_y": jnp.array(1.0),
             "beta": jnp.array(0.0),
             "alpha": jnp.array([0.0] * self.J),
+        }
+
+    def to_inference_params(self, model_params: ModelParams) -> InfParams:
+        return {
+            "sigma_y": self.sigma_tx.to_unconstrained(model_params["sigma_y"]),
+            "beta": model_params["beta"],
+            "alpha": model_params["alpha"],
+        }
+
+    def to_model_params(self, inf_params: InfParams) -> ModelParams:
+        return {
+            "sigma_y": self.sigma_tx.to_constrained(inf_params["sigma_y"]),
+            "beta": inf_params["beta"],
+            "alpha": inf_params["alpha"],
         }