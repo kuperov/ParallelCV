fec020895b32011f4b1919d5b5eca737b0f59f1f
Alex Cooper <alex@acooper.org>
2023-03-10 09:48:08 +1100

    bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def _parse_requirements(req_path):
 
 setup(
     name="pcv",
-    version="0.0.5",
+    version="0.0.6",
     description="Parallel MCMC diagnostic experiment",
     long_description=long_description,
     author="Alex Cooper",
