37de6226102baa0307d1aca3b3344d867f7ccaae
Alex Cooper <alex@acooper.org>
2023-03-21 13:43:59 +1100

    rhat threshold

--- pcv/rules.py
+++ pcv/rules.py
@@ -12,11 +12,11 @@ STOP_CONCLUSIVE = 1
 STOP_INCONCLUSIVE = 2
 
 
-def make_positive_rule(num_folds: int, level=0.95) -> Callable:
+def make_positive_rule(num_folds: int, level=0.95, max_rhat=1.1) -> Callable:
     qtile = 1. - 0.5*(1-level)
     pos_tcrit = tfd.StudentT(df=num_folds+2, loc=0., scale=1.).quantile(qtile)
     def rule(elpd_diff, diff_cvse, model_mcse, model_ess, num_folds, num_samples, model_rhats):
-        prereq = (jnp.max(model_rhats) < 1.05) and jnp.all(model_ess > 100*num_folds)
+        prereq = (jnp.max(model_rhats) < max_rhat) and jnp.all(model_ess > 100*num_folds)
         pos = jnp.abs(elpd_diff/jnp.sqrt(jnp.sum(model_mcse**2) + diff_cvse**2)) > pos_tcrit
         res = jnp.where(prereq,
                         jnp.where(pos, STOP_CONCLUSIVE, CONTINUE),
@@ -25,12 +25,12 @@ def make_positive_rule(num_folds: int, level=0.95) -> Callable:
     return rule
 
 
-def make_positive_negative_rule(num_folds: int, level=0.95) -> Callable:
+def make_positive_negative_rule(num_folds: int, level=0.95, max_rhat=1.1) -> Callable:
     qtile = 1. - 0.5*(1-level)
     pos_tcrit = tfd.StudentT(df=num_folds+2, loc=0., scale=1.).quantile(qtile)
     neg_tcrit = tfd.StudentT(df=num_folds, loc=0., scale=1.).quantile(qtile)
     def rule(elpd_diff, diff_cvse, model_mcse, model_ess, num_folds, num_samples, model_rhats):
-        prereq = (jnp.max(model_rhats) < 1.05) and jnp.all(model_ess > 100*num_folds)
+        prereq = (jnp.max(model_rhats) < max_rhat) and jnp.all(model_ess > 100*num_folds)
         pos = jnp.abs(elpd_diff/jnp.sqrt(jnp.sum(model_mcse**2) + diff_cvse**2)) > pos_tcrit
         neg = jnp.abs(elpd_diff/diff_cvse) < neg_tcrit
         res = jnp.where(prereq,
